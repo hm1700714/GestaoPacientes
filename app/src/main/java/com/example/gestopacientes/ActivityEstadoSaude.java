71acbaeac6a837995e48a4b254328fba7678a594
Henrique Moreira <1700714@sal.ipg.pt>
2020-04-21 17:18:50 +0100

    Criação do menu CRUD para o Doente.

---
+++ app/src/main/java/com/example/gestopacientes/ActivityEstadoSaude.java
@@ -0,0 +1,14 @@
+package com.example.gestopacientes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityEstadoSaude extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estado_saude);
+    }
+}
