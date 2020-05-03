e22f447b7bfd4ab348eeee6d27d1071e550b2577
Henrique Moreira <1700714@sal.ipg.pt>
2020-05-03 14:38:20 +0100

    Criação de nomas atividades para o CRUD

---
+++ app/src/main/java/com/example/gestopacientes/ActivityAdicionarEstadoSaude.java
@@ -0,0 +1,14 @@
+package com.example.gestopacientes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAdicionarEstadoSaude extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_estado_saude);
+    }
+}
