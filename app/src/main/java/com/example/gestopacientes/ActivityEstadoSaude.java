b4ae14ae64418221800a91218dba96544bab0e5a
Henrique Moreira <1700714@sal.ipg.pt>
2020-05-06 09:21:42 +0100

    Corrigidos alguns erros no layout

--- app/src/main/java/com/example/gestopacientes/ActivityEstadoSaude.java
+++ app/src/main/java/com/example/gestopacientes/ActivityEstadoSaude.java
@@ -2,7 +2,9 @@ package com.example.gestopacientes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityEstadoSaude extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ActivityEstadoSaude extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estado_saude);
+    }
+
+    public void passaAdicionarEstadoSaude (View view){
+
+        Intent intent = new Intent(this, ActivityAdicionarEstadoSaude.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/gestopacientes/ActivitySintomas.java
+++ app/src/main/java/com/example/gestopacientes/ActivitySintomas.java
@@ -2,7 +2,9 @@ package com.example.gestopacientes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivitySintomas extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class ActivitySintomas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sintomas);
+    }
+
+    public void passaAdicionarSintomas (View view){
+
+        Intent intent = new Intent(this, ActivityAdicionarSintomas.class);
+        startActivity(intent);
     }
 
 }
